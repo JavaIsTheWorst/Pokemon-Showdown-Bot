7e5066095566359cf8c467ad7de3e73f9bd52920
JavaIsTheWorst <thisisscioly@gmail.com>
2017-08-15 13:33:40 -0400

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -1,28 +1,27 @@
 {-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 module Main where
 
-import           Control.Concurrent   (forkIO, threadDelay)
-import           Network.HTTP         (simpleHTTP, postRequestWithBody, getResponseBody)
-import           Network.HTTP.Base    (urlEncode, rspReason)
-import           Network.Socket       (withSocketsDo)
-import           Control.Monad        (forever, unless)
-import           Control.Monad.Trans  (liftIO)
-import           Data.Aeson           (eitherDecode, ToJSON, FromJSON, toEncoding, genericToEncoding, defaultOptions)
-import           Data.List            (isPrefixOf)
-import           Data.Text            (Text)
-import           GHC.Generics         (Generic)
-import           GHC.IO.Encoding      (setLocaleEncoding, utf8)
-import           Data.Text.Encoding   (encodeUtf8)
-import           System.Random        (randomRIO)
-import qualified Data.ByteString.Lazy as B
-import qualified Data.Text            as T
-import qualified Data.Text.IO         as T
-import qualified Network.WebSockets   as WS
+import           Control.Concurrent           (forkIO, threadDelay)
+import           Network.HTTP                 (simpleHTTP, postRequestWithBody, getResponseBody)
+import           Network.HTTP.Base            (urlEncode, rspReason)
+import           Network.Socket               (withSocketsDo)
+import           Control.Monad                (forever, unless)
+import           Control.Monad.Trans          (liftIO)
+import           Data.Aeson                   (eitherDecode, ToJSON, FromJSON, toEncoding, genericToEncoding, defaultOptions)
+import           Data.List                    (isPrefixOf, isInfixOf)
+import           Data.Text                    (Text)
+import           GHC.Generics                 (Generic)
+import           GHC.IO.Encoding              (setLocaleEncoding, utf8)
+import           Data.Text.Encoding           (encodeUtf8)
+import           System.Random                (Random, random, randomR, randomRIO, randomIO)
+import qualified System.IO.Strict             as S
+import qualified Data.ByteString.Lazy         as B
+import qualified Data.Text                    as T
+import qualified Data.Text.IO                 as T
+import qualified Network.WebSockets           as WS
 
 import qualified Config
-
-send :: WS.Connection -> T.Text -> IO ()
-send = WS.sendTextData
+import           Util
 
 app :: WS.ClientApp ()
 app conn = do
@@ -51,52 +50,104 @@ parse conn s = print s >> parse' conn s
             in  do
               httpResponse <- simpleHTTP (postRequestWithBody Config.actionUrl "application/x-www-form-urlencoded" postData)
               (_:jsonData) <- getResponseBody httpResponse
-              let loginCommand = "|/trn " ++ Config.username ++ ",0," ++ getAssertion jsonData
-              send conn $ T.pack loginCommand
+              let loginCommand = "/trn " ++ Config.username ++ ",0," ++ getAssertion jsonData
+              useCommand conn $ T.pack loginCommand
               startUpActions conn
-          | "|pm" `T.isPrefixOf` s = do
-            appendFile "log.txt" $ T.unpack s
-            commandParse conn "pm" (messageList !! 2) $ T.intercalate "|" (tail.tail.tail.tail$messageList)
+          | "|pm" `T.isPrefixOf` s = commandParse conn "pm" (messageList !! 2) $ T.intercalate "|" (tail.tail.tail.tail$messageList)
           | otherwise = return ()
             where messageList = T.splitOn "|" s
 
+setSetting :: String -> String -> IO ()
+setSetting setting newSetting = do
+  settings <- S.readFile "settings.txt"
+  let filteredSettingsList = filter (not . (setting `isPrefixOf`)) $ lines settings
+  let updatedSettings = unlines $ (setting ++ ": " ++ newSetting) : filteredSettingsList
+  writeFile "settings.txt" updatedSettings
+
+getSetting :: String -> IO (Maybe String)
+getSetting setting = do
+  settings <- S.readFile "settings.txt"
+  let searchedSetting = filter (setting `isPrefixOf`) $ lines settings
+  if null searchedSetting
+    then return Nothing
+    else return . Just . drop (length setting + 2) . head $ searchedSetting
+
+data Choice = Yes | No deriving (Enum, Bounded)
+
+instance Random Choice where
+  random g =
+    case randomR (fromEnum (minBound :: Choice), fromEnum (maxBound :: Choice)) g of
+      (r,g') -> (toEnum r,g')
+  randomR (a,b) g =
+    case randomR (fromEnum a, fromEnum b) g of
+      (r,g') -> (toEnum r,g')
+
+instance Show Choice where
+  show Yes = "Yes."
+  show No = "No."
+
 commandParse :: WS.Connection -> T.Text -> T.Text -> T.Text -> IO ()
 commandParse conn room username message
-  | "should " `T.isPrefixOf` (T.toLower message) =
-    if room == "pm"
-      then send conn $ "|/w " `T.append` username `T.append` ", No."
-      else send conn $ room `T.append` "|No."
+  | "should " `T.isPrefixOf` (T.toLower message) = do
+    setting <- getSetting "should auto-reply no"
+    if setting == Nothing || setting /= Just "True"
+      then return ()
+      else if room == "pm"
+        then pm conn username "No."
+        else say conn room "No."
+  | "stop saying no" `T.isInfixOf` (T.toLower message) && T.tail username == T.pack Config.owner =
+    setSetting "should auto-reply no" "False"
+  | "start saying no" `T.isInfixOf` (T.toLower message) && T.tail username == T.pack Config.owner =
+    setSetting "should auto-reply no" "True"
   | "say " `T.isPrefixOf` (T.toLower message) && T.tail username == T.pack Config.owner =
     send conn (T.tail.T.tail.T.tail.T.tail$message)
-  | "github please" `T.isPrefixOf` (T.toLower message) =
-    send conn $ "|/w " `T.append` username `T.append` ", https://github.com/JavaIsTheWorst/Pokemon-Showdown-Bot"
+  | "github please" `T.isInfixOf` (T.toLower message) =
+    pm conn username "https://github.com/JavaIsTheWorst/Pokemon-Showdown-Bot"
   | "what is java" `T.isPrefixOf` (T.toLower message) = do
     forkIO $ do
-      send conn $ "|/w " `T.append` username `T.append` ", Java is a general-purpose (aka vaguely defined purpose) computer programming language (voodoo) that is concurrent (bad at multi-tasking), class-based (tries to be well structured), object-oriented (cannot process abstract ideas), and specifically designed to have as few implementation dependencies"
-      send conn $ "|/w " `T.append` username `T.append` ", as possible (isolated from reality)."
+      pm conn username "Java is a general-purpose (aka vaguely defined purpose) computer programming language (voodoo) that is concurrent (bad at multi-tasking), class-based (tries to be well structured), object-oriented (cannot process abstract ideas), and specifically designed to have as few implementation dependencies"
+      pm conn username "as possible (isolated from reality)."
       threadDelay $ 1*1000*1000
-      send conn $ "|/w " `T.append` username `T.append` ", It is intended to let application developers \"write once, run anywhere\", meaning that compiled Java code can run on all platforms that support Java without the need for recompilation"
-      send conn $ "|/w " `T.append` username `T.append` ", (making the arms of corporate conspiracies to quietly take over the modern society stretch all that farther)."
+      pm conn username "It is intended to let application developers \"write once, run anywhere\", meaning that compiled Java code can run on all platforms that support Java without the need for recompilation"
+      pm conn username "(making the arms of corporate conspiracies to quietly take over the modern society stretch all that farther)."
     return ()
   | "ab" == message =
-    send conn $ "|/w " `T.append` username `T.append` ", Identity confirmed: " `T.append` username
+    pm conn username $ ", Identity confirmed: " `T.append` username
+  | T.pack (Config.username ++ ", choose") `T.isPrefixOf` (T.toLower message) = do
+    setting <- getSetting "should choose"
+    if setting == Nothing || setting /= Just "True"
+      then return ()
+      else do
+        choice <- randomIO :: IO Choice
+        let textChoice = T.pack $ show choice
+        if room == "pm"
+          then pm conn username textChoice
+          else say conn room textChoice
+  | "stop choosing" `T.isInfixOf` (T.toLower message) && T.tail username == T.pack Config.owner =
+    setSetting "should choose" "False"
+  | "start choosing" `T.isInfixOf` (T.toLower message) && T.tail username == T.pack Config.owner =
+    setSetting "should choose" "True"
   | otherwise =
     if room == "pm" && T.tail username /= T.pack Config.username
       then do
         forkIO $ do
-          m <- randomRIO (1,20) :: IO Int
-          threadDelay $ m*1000*1000
-          n <- randomRIO (1,4) :: IO Int
-          send conn $ "|/w " `T.append` username `T.append` "," `T.append` T.replicate n "?"
+          respond <- randomRIO (1,5) :: IO Int
+          if respond == 1
+            then return ()
+            else do
+              m <- randomRIO (1,20) :: IO Int
+              threadDelay $ m*1000*1000
+              n <- randomRIO (1,4) :: IO Int
+              pm conn username $ T.replicate n "?"
         return ()
       else return ()
 
 startUpActions :: WS.Connection -> IO ()
 startUpActions conn = do
-  send conn "|/avatar 120"
+  useCommand conn "/avatar 120"
   threadDelay $ 600 * 1000
-  mapM_ ((>> threadDelay (600 * 1000)) . send conn . T.append "|/j ") Config.rooms
-  mapM_ ((>> threadDelay (600 * 1000)) . send conn . (flip T.append) ", Harassed." . T.append "|/w ") Config.harassList
+  mapM_ ((>> threadDelay (600 * 1000)) . useCommand conn . ("/j " `T.append`)) Config.rooms
+  mapM_ ((>> threadDelay (600 * 1000)) . ($ "Harassed.") . pm conn) Config.harassList
 
 data User = User {
   userid :: String,
