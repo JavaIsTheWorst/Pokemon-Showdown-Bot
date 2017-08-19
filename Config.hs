f1d1512b4bb412e37f8f97a32442ab9b0158610d
JavaIsTheWorst <thisisscioly@gmail.com>
2017-08-19 17:00:46 -0400

    Update Config.hs

--- Config.hs
+++ Config.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Config where
 
-import qualified Data.Text as T
+import qualified Data.Text                 as T
+import qualified System.Console.ANSI       as ANSI
+import qualified System.Console.ANSI.Types as ANSI
+
+import           Util
 
 server :: String
 server = "sim.smogon.com"
@@ -32,3 +36,24 @@ rooms = --List of rooms to join
 
 harassList :: [T.Text]
 harassList = --List of username to PM upon startup
+
+logColor :: ANSI.Color
+logColor = ANSI.Blue
+
+hangmanColor :: ANSI.Color
+hangmanColor = ANSI.White
+
+unoColor :: ANSI.Color
+unoColor = ANSI.Green
+
+commandColor :: ANSI.Color
+commandColor = ANSI.White
+
+connectedColor :: ANSI.Color
+connectedColor = ANSI.White
+
+loginColor :: ANSI.Color
+loginColor = ANSI.White
+
+startUpColor :: ANSI.Color
+startUpColor = ANSI.White
