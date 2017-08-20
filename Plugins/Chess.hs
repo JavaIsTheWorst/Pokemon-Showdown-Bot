76dda9f2aaae2c2d441bfffaad7b8f64bbb51a4a
JavaIsTheWorst <thisisscioly@gmail.com>
2017-08-20 14:10:29 -0400

    Create Chess.hs

---
+++ Plugins/Chess.hs
@@ -0,0 +1,8 @@
+module Plugins.Chess where
+
+--This feature does not yet exist, but it may soon!
+
+{-
+onInitiateMessage :: MVar () -> WS.Connection -> T.Text -> T.Text -> T.Text -> IO ()
+onInitiateMessage lock conn room playerOne playerTwo =
+-}
